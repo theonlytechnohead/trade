b444e38b11afb7493f49566a0155b81c261773d0
Craig Anderson <cranderson0110@gmail.com>
2018-04-15 13:21:38 +1200

    Cleaned imports and added to manifest

--- app/src/main/java/com/jcinc/trade/ConnectionClass.java
+++ app/src/main/java/com/jcinc/trade/ConnectionClass.java
@@ -1,31 +1,15 @@
 package com.jcinc.trade;
 
 import android.annotation.SuppressLint;
-import android.content.Context;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.os.StrictMode;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 public class ConnectionClass extends AppCompatActivity {
 
