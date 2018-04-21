a621a111bd2f17d0f7cce864faff752fa1da5509
Jakob Leech <rockyman218@gmail.com>
2018-04-21 21:28:16 +1200

    done shit

--- app/src/main/java/com/jcinc/trade/MainActivity.java
+++ app/src/main/java/com/jcinc/trade/MainActivity.java
@@ -1,6 +1,7 @@
 package com.jcinc.trade;
 
 import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.annotation.NonNull;
@@ -16,6 +17,7 @@ import org.jsoup.nodes.Document;
 public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
+    public  Document docFinal;
 
     //RequestQueue queue = Volley.newRequestQueue(this);
 
@@ -50,6 +52,39 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
+        final String url = "http://35.197.91.51/api.php";
+
+        AsyncTask execute = new AsyncTask() {
+
+            @Override
+            protected Document doInBackground(Object[] objects) {
+                Document doc = null;
+                try {
+                    doc = Jsoup.connect(url).get();
+
+                } catch (Exception e) {
+                    doc = null;
+                }
+
+                return doc;
+            }
+
+            @Override
+            protected void onPostExecute(Object result) {
+                if (result != null) {
+                    textView.setText("Loading website");
+                    docFinal = (Document) result;
+                    POST();
+
+                } else {
+                    textView.setText("Error");
+                }
+            }
+
+        };
+
+        execute.execute();
+
 
         new CountDownTimer(1000, 1000) {
             @Override
@@ -60,7 +95,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onFinish() {
                 setTitleText("Home");
-                textView.setText(POST() + " Test");
             }
         }.start();
     }
@@ -69,16 +103,17 @@ public class MainActivity extends AppCompatActivity {
 
 
     public String POST () {
-        String data;
-        String url = "http://35.197.91.51/api.php";
-        try {
-            Document doc = Jsoup.connect("http://35.197.91.51/api.php").get();
-            data = doc.text();
-        }
-        catch (Exception e){
-            e.printStackTrace();
-            data = String.valueOf(e.getCause());
-        }
+        textView.setText(docFinal.text());
+        //String data;
+        //String url = "http://35.197.91.51/api.php";
+        //try {
+        //    Document doc = Jsoup.connect("http://35.197.91.51/api.php").get();
+        //    data = doc.text();
+        //}
+        //catch (Exception e){
+        //    e.printStackTrace();
+        //    data = String.valueOf(e.getCause());
+        //}
 
         // StringRequest postRequest = new StringRequest(Request.Method.POST, url,
         //        new Response.Listener<String>() {
@@ -105,8 +140,10 @@ public class MainActivity extends AppCompatActivity {
         //    }
         //};
         //queue.add(postRequest);
-        return data;
+        //return data;
+        return null;
     }
+
 
     private void setTitleText (String titleText) {
         ActionBar actionBar = getSupportActionBar();
